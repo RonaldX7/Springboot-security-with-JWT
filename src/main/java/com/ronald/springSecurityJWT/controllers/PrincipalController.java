8247045fe5b06352195f1613daa006f83897f65a
RonaldX7 <ronaldjair0705@gmail.com>
2024-09-16 12:27:50 -0500

    nuevos cambios

--- src/main/java/com/ronald/springSecurityJWT/controllers/PrincipalController.java
+++ src/main/java/com/ronald/springSecurityJWT/controllers/PrincipalController.java
@@ -1,13 +1,5 @@
 package com.ronald.springSecurityJWT.controllers;
 
-import com.ronald.springSecurityJWT.dtos.UserDTO;
-import com.ronald.springSecurityJWT.entities.ERole;
-import com.ronald.springSecurityJWT.entities.RoleEntity;
-import com.ronald.springSecurityJWT.entities.UserEntity;
-import com.ronald.springSecurityJWT.repositories.UserRepository;
-import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
@@ -19,11 +11,13 @@ public class PrincipalController {
 
 
     @GetMapping("/hello")
+    @PreAuthorize("permitAll()")
     public String hello(){
         return "Hello World not secured";
     }
 
     @GetMapping("/helloSecured")
+    @PreAuthorize("hasAuthority('CREATE')")
     public String helloSecured(){
         return "Hello World secured";
     }

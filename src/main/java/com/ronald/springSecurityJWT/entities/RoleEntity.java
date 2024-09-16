7b0fbc8b1f5e650b6cd33d2a5eb80e2eec87947d
RonaldX7 <ronaldjair0705@gmail.com>
2024-09-16 16:30:12 -0500

    nuevos cambios

---
+++ src/main/java/com/ronald/springSecurityJWT/entities/RoleEntity.java
@@ -0,0 +1,4 @@
+package com.ronald.springSecurityJWT.entities;
+
+public class RoleEntity {
+}
--- src/main/java/com/ronald/springSecurityJWT/entities/UserEntity.java
+++ src/main/java/com/ronald/springSecurityJWT/entities/UserEntity.java
@@ -18,10 +18,20 @@ public class UserEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    @Column(unique = true)
     private String username;
 
     private String password;
 
+    @Column(name = "is_enable")
     private boolean isEnabled;
 
+    @Column(name = "account_No_Expired")
+    private boolean accountNoExpired;
+
+    @Column(name = "account_No_Locked")
+    private boolean accountNoLocked;
+
+    @Column(name = "credentials_No_Expired")
+    private boolean credentialsNoExpired;
 }

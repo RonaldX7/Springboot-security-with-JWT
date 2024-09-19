b59543e129338cc9692280153e8a14d9b071a8ad
RonaldX7 <ronaldjair0705@gmail.com>
2024-09-19 10:59:57 -0500

    nuevos cambios

--- src/main/java/com/ronald/springSecurityJWT/entities/RoleEntity.java
+++ src/main/java/com/ronald/springSecurityJWT/entities/RoleEntity.java
@@ -3,6 +3,9 @@ package com.ronald.springSecurityJWT.entities;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Setter
 @Getter
 @AllArgsConstructor
@@ -19,4 +22,10 @@ public class RoleEntity {
     @Column(name = "role_name")
     @Enumerated(EnumType.STRING)
     private RoleEnum roleEnum;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "role_permissions",
+            joinColumns = @JoinColumn(name = "role_id"),
+            inverseJoinColumns = @JoinColumn(name = "permission_id"))
+    private Set<PermissionEntity> permissionList = new HashSet<>();
 }
